f7f2b10b074ef3ba1750b82315670decf736e256
sujumayas <sujumayas@gmail.com>
2024-07-25 17:56:39 -0500

    rewriter

---
+++ git-author-rewrite.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="pickle_rick@Esens-MacBook-Pro.local"
+CORRECT_NAME="sujumayas"
+CORRECT_EMAIL="sujumayas@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
